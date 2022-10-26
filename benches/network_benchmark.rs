fb9bc69565d9e6ac01cd7ff0a2dcedccc6967da3
maya_k <maya.kleinstein@mail.huji.ac.il>
2022-10-26 14:22:41 +0300

    first bench ever working :)

--- benches/network_benchmark.rs
+++ benches/network_benchmark.rs
@@ -2,11 +2,15 @@ use bbs::network::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 
-// Things I want to test: Time to generate a ticket, time to verify a ticket and signature, Time t
+// Things I want to test: Time to generate a ticket, time to verify a ticket and signature, Time to decrypt a packet
 
 
 fn generate_packet_benchmark(c: &mut Criterion) {
     let data = black_box(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     let network = black_box(Network::new(5));
     let client = black_box(Client::new(&network));
+    c.bench_function("generate_packet", |b| b.iter(|| generate_packet(data.clone(), &client, &network)));
 }
+
+criterion_group!(benches, generate_packet_benchmark);
+criterion_main!(benches);
