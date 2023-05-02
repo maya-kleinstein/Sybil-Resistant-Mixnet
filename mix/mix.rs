d08396075f222c5ee2847155935fbb490df58332
maya_k <maya.kleinstein@mail.huji.ac.il>
2023-05-02 12:34:26 +0300

    oops

--- mix/mix.rs
+++ mix/mix.rs
@@ -1,10 +1,8 @@
 use bbs::mix::*;
-use futures::TryFutureExt;
-use std::io;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let arg = std::env::args().nth(1).expect("no pattern given");
     let id: u16 = arg.parse().unwrap();
-    run_mix(id);
+    run_mix(id).await
 }
