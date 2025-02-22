49aba1ea376dcd27d3f107a871882f8ff7011a68
maya-kleinstein <mayak144@gmail.com>
2025-02-22 16:41:25 +0200

    changed how we find initial mix IP's to use SLURM_PROCID

--- src/marshal/ips.rs
+++ src/marshal/ips.rs
@@ -7,15 +7,17 @@ use std::time::Duration;
 
 /// Get's all mixes IP's sorted, and my mix's index
 pub fn init_mix_ips() -> io::Result<(Vec<IpAddr>, u16)> {
-    let my_ip = write_my_ip_to_file()?;
+    let my_id = std::env::var("SLURM_PROCID").unwrap();
+    let my_ip = write_my_ip_to_file(&my_id)?;
     let ips = get_all_ips_from_files()?;
 
-    let index = ips.iter().position(|&r| r == my_ip).unwrap();
+    let index = my_id.parse::<u16>().unwrap();
 
+    assert!(ips[index as usize] == my_ip);
     println!("All IPs: {:?}", ips);
     println!("My ID: {}", index);
 
-    Ok((ips, index as u16))
+    Ok((ips, index))
 }
 
 // Get all mixes IP's from files, sorted
@@ -27,13 +29,12 @@ pub fn get_all_ips_from_files() -> std::io::Result<Vec<IpAddr>> {
         sleep(Duration::from_secs(1));
         ips = get_cur_ip_files(&*IPS_FOLDER)?;
     }
-    ips.sort();
     Ok(ips)
 }
 
-pub fn write_my_ip_to_file() -> io::Result<IpAddr> {
+pub fn write_my_ip_to_file(my_id: &String) -> io::Result<IpAddr> {
     let my_ip = get_my_ip()?;
-    let filename = format!("{}{}", *IPS_FOLDER, my_ip);
+    let filename = format!("{}{}", *IPS_FOLDER, my_id);
     serialize_data_to_file::<IpAddr>(&my_ip, &filename).unwrap();
     Ok(my_ip)
 }
@@ -45,23 +46,22 @@ pub fn get_my_ip() -> io::Result<IpAddr> {
 }
 
 pub fn get_cur_ip_files(dir: &str) -> std::io::Result<Vec<IpAddr>> {
-    let mut ips: Vec<IpAddr> = Vec::new();
+    let mut ips: Vec<(IpAddr, u16)> = Vec::new();
     for entry in fs::read_dir(format!("{}{}", *BASE_FOLDER, dir))? {
         let entry = entry?;
         let path = entry.path();
         if path.is_file() {
-            if let Some(filename) = path.file_name() {
-                if let Some(filename_str) = filename.to_str() {
-                    let ip = filename_str.parse::<IpAddr>();
-                    if ip.is_ok() {
-                        ips.push(ip.unwrap());
-                    }
-                }
-            }
+            let filename = path.file_name().unwrap().to_str().unwrap();
+            let id = filename.parse::<u16>().unwrap();
+            let ips_filename = format!("{}{}", *IPS_FOLDER, filename);
+            let ip = deserialize_data_from_file::<IpAddr>(&ips_filename).unwrap();
+            ips.push((ip, id));
         }
     }
-    ips.sort();
-    Ok(ips)
+    // sort ips based on the id value
+    ips.sort_by_key(|k| k.1);
+    let ordered_ips: Vec<IpAddr> = ips.iter().map(|(ip, _)| *ip).collect();
+    Ok(ordered_ips)
 }
 
 pub fn delete_ip_files() {
@@ -88,31 +88,23 @@ mod test {
     fn ips_test() {
         // Write random IP's as files to IP folder
         let mut rand_ips: Vec<IpAddr> = Vec::new();
-        for _ in 0..((*NUM_MIXES - 1) as usize) {
+
+        for _ in 0..(*NUM_MIXES as usize) {
             let r = rand::random();
             let ip = Ipv4Addr::new(r, r, r, r);
             rand_ips.push(IpAddr::V4(ip));
         }
-        for ip in rand_ips.iter() {
-            let filename = format!("{}{}", *IPS_FOLDER, ip);
-            serialize_data_to_file::<IpAddr>(&ip, &filename).unwrap();
+
+        for i in 0..rand_ips.len() {
+            let filename = format!("{}{}", *IPS_FOLDER, i);
+            serialize_data_to_file::<IpAddr>(&rand_ips[i], &filename).unwrap();
         }
 
-        // Write my IP to the IP folder
-        let my_ip = get_my_ip().unwrap();
-        let filename = format!("{}{}", *IPS_FOLDER, my_ip);
-        serialize_data_to_file::<IpAddr>(&my_ip, &filename).unwrap();
-
         // Get all IP's from the IP folder
-        let mut ips = get_all_ips_from_files().unwrap();
+        let ips = get_all_ips_from_files().unwrap();
         assert_eq!(ips.len(), *NUM_MIXES as usize);
 
-        ips.sort();
-        let index = ips.iter().position(|&r| r == my_ip).unwrap();
-        assert_eq!(ips[index], my_ip);
-
         println!("All IPs: {:?}", ips);
-        println!("My IP's index: {}", index);
 
         // Empty IP folder
         delete_ip_files();
